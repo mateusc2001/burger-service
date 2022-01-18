816af0596f8c43bc7284cd47d5fcfb569e4d1c84
mateusc2001 <mateusc2001@gmail.com>
2022-01-17 22:27:20 -0300

    AJustando apontamento para imagens

--- src/server.ts
+++ src/server.ts
@@ -5,13 +5,12 @@ import app from "./app";
 
 const port = 2020;
 app.listen(process.env.PORT || port);
-
 app.get('/home', (req, res) => {
-    res.sendFile(`/Users/testemtech/projetos/portifolio/backend/front/index.html`);
+    res.sendFile(`${__dirname}/front/index.html`);
 });
 
 app.get('/image/:fileName', (req, res) => {
     const fileName = req.params.fileName;
-    res.sendFile(`/Users/testemtech/projetos/portifolio/backend/assets/${fileName}`);
+    res.sendFile(`${__dirname}/assets/${fileName}`);
 });
 console.log(`Ambiente de desenvolvimento rodando: http://localhost:${port}`)
